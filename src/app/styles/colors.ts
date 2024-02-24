f0319c5f2ed06b8f6dbf6358ac5347069c747c05
Justin Rich <justinrich2008@gmail.com>
2024-02-24 12:40:26 -0800

    Initial commit from Create Next App

---
+++ src/app/styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  background: {
+    primary: "#1e0c30",
+    secondary: "#ffffff",
+    tertiary: "#3d165e",
+    quaternary: "#a03dff",
+  },
+  border: "#e5e8ea",
+  text: {
+    primary: "#ffffff",
+    secondary: "#af7cdd",
+  },
+  elementBackground: {
+    primary: "#3d165e",
+    secondary: "#a03dff",
+  },
+} as const;
