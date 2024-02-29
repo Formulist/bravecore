5fc42a0e9809e62c432d199d26d8864e0fb8e4d6
Justin Rich <justinrich2008@gmail.com>
2024-02-29 09:04:53 -0800

    save

--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,12 +1,14 @@
 import { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import client from "graphql-request";
-
+const isProd = !!process.env?.NODE_ENV?.includes("prod");
 export const gqlClient = <R = any, V = any>(
   document: TypedDocumentNode<R, V>,
   variables: Record<string, any>
 ) =>
   client({
     document,
-    url: "http://localhost:8080/graphql",
+    url: isProd
+      ? process.env.NEXT_PUBLIC_GATEWAY_URL ?? ""
+      : "http://localhost:8080/graphql",
     variables,
   });
