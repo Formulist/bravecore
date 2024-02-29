5caeaa44b01c0132f38964d1c59d5d02e252b912
Justin Rich <justinrich2008@gmail.com>
2024-02-29 12:32:59 -0800

    proxy add

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  env: {
+    NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/graphql",
+        destination:
+          process?.env?.NEXT_PUBLIC_GATEWAY_URL ??
+          "http://localhost:8080/graphql",
+      },
+    ];
   },
 };
 
--- src/gql/client.ts
+++ src/gql/client.ts
@@ -6,7 +6,6 @@ export const gqlClient = <R = any, V = any>(
 ) =>
   client({
     document,
-    url:
-      process?.env?.NEXT_PUBLIC_GATEWAY_URL ?? "http://localhost:8080/graphql",
+    url: "/api/graphql",
     variables,
   });
